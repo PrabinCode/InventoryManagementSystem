8fb832fc4817e95810ac8a6b1157c2ecca972f53
prabin <pcstha01@gmail.com>
2020-03-18 13:06:46 +0545

    Inital Project Added

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IMSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IMSystem.WebApiApplication" Language="C#" %>
